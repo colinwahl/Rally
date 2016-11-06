f9296ba610c93802ca9579b798721cc396d5d7a7
Colin Wahl <colin.t.wahl@gmail.com>
2016-11-06 03:50:21 -0500

    yo yo yo

--- src/Main.js
+++ src/Main.js
@@ -3,36 +3,66 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput
+  TextInput,
+  ListView
 } from 'react-native';
 import MapView from 'react-native-maps'
 import { Button, Divider } from 'react-native-material-design'
 import SideMenu from 'react-native-side-menu'
 
 
-import { Router, Scene, Actions } from 'react-native-router-flux'
+import { Router, Scene, Actions, Subheader } from 'react-native-router-flux'
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
     flex: 1,
-    alignItems: 'center',
-    padding: 10
+    padding: 10,
+    flexDirection: 'column',
+    backgroundColor: '#e1e9f0'
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 0.5,
+    height: 300
   },
+  listview: {
+    flex: 0.5,
+    height: 300,
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  username: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  bordered: {
+    borderColor: '#6ed3cf',
+    borderWidth: 5
+  },
+  menu: {
+    padding: 10
+  },
+  panic: {
+    color: '#e62739',
+    fontWeight: 'bold'
+  },
+  name: {
+    color: '#6ed3cf'
+  }
 })
 
 export default class Main extends Component {
   constructor(props) {
     super(props)
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
     this.state = {
       users: {},
       lat: this.props.lat,
       lon: this.props.lon,
       panic: false,
-      status: ""
+      status: "",
+      datasource: this.ds.cloneWithRows([])
     };
     this.update = this.update.bind(this)
     this.updateGroup = this.updateGroup.bind(this)
@@ -87,7 +117,16 @@ export default class Main extends Component {
       })
     })
     .then(response => JSON.parse(response._bodyInit))
-    .then(data => this.setState({users: data}))
+    .then(data => {
+      var a = Object.keys(this.state.users).filter(user => this.state.users[user].status !== "" || this.state.users[user].panic).map(user => {
+        return {
+          username: user,
+          status: this.state.users[user].status,
+          panic: this.state.users[user].panic
+        }
+      })
+      this.setState({users: data, datasource: this.ds.cloneWithRows(a)})
+    })
     .catch(e => console.warn("here", JSON.stringify(e)))
     .done()
   }
@@ -122,12 +161,11 @@ export default class Main extends Component {
       })
     })
     .done()
-    this.setState({status: ""})
   }
 
  render() {
    var menu = (
-     <View>
+     <View style={styles.menu}>
         <Text>Need help? Let people know!</Text>
         <Button text={this.state.panic ? "I am ok!" : "Send Help!"} raised={true} onPress={()=> this.panic()} />
         <Divider />
@@ -139,6 +177,7 @@ export default class Main extends Component {
    return (
      <SideMenu menu={menu}>
      <View style={styles.container}>
+       <View style={styles.bordered}>
        <MapView
          style={styles.map}
          region={{
@@ -152,18 +191,10 @@ export default class Main extends Component {
               coordinate={{latitude: this.state.lat, longitude: this.state.lon}}
               title={this.props.username}
               description="You are here."
-              pinColor="ffff22"
+              pinColor="#ffff22"
             />
           {Object.keys(this.state.users).map(user => {
-              if (user !== this.props.username && this.state.users[user].panic) {
-              return (<MapView.Marker
-                coordinate={{latitude: this.state.users[user].lat, longitude: this.state.users[user].lon}}
-                title={user}
-                description={this.state.users[user].status}
-                image={require('./panic.png')}
-              />)
-              }
-              else if (user !== this.props.username) {
+            if (user !== this.props.username) {
                 return (<MapView.Marker
                   coordinate={{latitude: this.state.users[user].lat, longitude: this.state.users[user].lon}}
                   title={user}
@@ -173,6 +204,19 @@ export default class Main extends Component {
               }
             )}
        </MapView>
+       </View>
+       <View style={styles.listview}>
+         <Text style={styles.username}>Status Feed</Text>
+         <Divider />
+         <ListView
+           dataSource={this.state.datasource}
+           renderRow={user => {
+             if (user.panic) {
+               return <View><Text style={styles.panic}>{user.username} needs help!</Text><Divider /></View>
+             }
+             return <View><Text style={styles.name}>{user.username}</Text><Text>{user.status}</Text><Divider /></View>
+           }} />
+       </View>
      </View>
    </SideMenu>
 
